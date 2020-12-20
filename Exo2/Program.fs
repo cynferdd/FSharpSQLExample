a1302c5ed22526bb46893d0095781fefc030c2f5
mathieu jolivet <cynferdd@gmail.com>
2020-12-20 16:36:48 +0100

    saving local dev

---
+++ Exo2/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+open XPlot.Plotly
+
+let [<Literal>] connectionString = "Server=(local);Database=CleemyDepenses;Trusted_Connection=True;MultipleActiveResultSets=true"
+
+[<EntryPoint>]
+let main argv =
+    use cmd = new SqlCommandProvider<"Select Date, Montant, UtilisateurId from Depenses WHERE UtilisateurId=@userId", connectionString>(connectionString)
+    cmd.Execute(userId = 1) |> Seq.iter(fun r -> printfn "%s %f" (r.Date.ToShortDateString()) r.Montant) 
+    0 // return an integer exit code
